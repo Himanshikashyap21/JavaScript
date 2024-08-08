baef9ecf6bba613f130b93d127277564f7ea7d2f
Himanshikashyap21 <himanshikashyap541@gmail.com>
2024-08-09 01:29:37 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(3+3) //we are using nodejs, not browser
+
+
+console.log(3+3); console.log("Himanshi") // code readability should be high
+
+let name="ahbs"
+let age=19
+let isLoggedIn=false
+let state;
+
+//==> Primitive datatypes
+//number=>2 to poer 53
+//bigit
+//string
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value is not assigned
+//symbol=use for uniqueness
+
+
+//object
+console.log(typeof "Himanshi"); //is a string
+console.log(typeof age); // is a number
+console.log(typeof null); //null is a object
+console.log(typeof undefined); // undefined
