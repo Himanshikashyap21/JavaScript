33e2f1c1fb4cabc9e46da4049bbee8012e4d1343
Himanshikashyap21 <himanshikashyap541@gmail.com>
2024-08-09 00:07:00 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12233 //no change
+let accountEmail = "himandhi13@gmail.com" // can change
+var accountPassword= "Jaipur" // can change
+accountCity = "Jaipur" 
+let  accountState;
+
+//accountId=2 //not allowed
+accountEmail="jndiew@gmail.com"
+accountPassword="12i3849"
+accountCity="Aligarh "
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
