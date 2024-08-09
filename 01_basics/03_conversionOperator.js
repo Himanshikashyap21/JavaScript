77e6c95c877869456e78f3fd6560a3c9526b74c9
Himanshikashyap21 <himanshikashyap541@gmail.com>
2024-08-09 15:54:07 +0530

    /conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,25 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+// "true" => 1; false=>0
+//"33abc"=>NaN (Not a Number)
+
+let isLoggedIn="sasx"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//"" =>false
+//"hced"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
